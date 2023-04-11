45b7a479a43fe47ed9f90c56638b80e761c702ee
João Teles <104568838+BROOK442@users.noreply.github.com>
2023-04-11 18:05:37 +0000

    wilson

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const containers = document.querySelectorAll("div.ia21-player")
 
-containers.forEach(el => {
-    const playPause = el.querySelector("button.play-pause")
-    const video = el.querySelector("video")
-    playPause.onclick = () => video.play()
+containers.forEach(container => { 
+    const btPlayPause = container.querySelector("button.play-pause")
+    const video = container.querySelector("video")
+    const timeline = container.querySelector(".dragbar.timeline")
+    const timelineDrag = timeline.querySelector(".draggable")
+    const timer = container.querySelector(".timer")
+    btPlayPause.addEventListener("click", () => video.play())
+    
+    video.currentTime = 55
 
-    playPause.addEventListener("click", () => alert("zzzz"))
+    video.addEventListener("timeupdate", () => {
+        const percent = (video.currentTime / video.duration) * 100
+        timelineDrag.style.setProperty("--percent", `${percent}%`) 
+        const min = Math.floor(video.currentTime / 60)
+        const seg = Math.floor(video.currentTime % 60)
+        console.log(seg)
+        timer.innerText = Math.floor(video.currentTime)
+    })
 })
