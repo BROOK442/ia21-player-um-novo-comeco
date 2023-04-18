6c04a3651d500084040345aed1c6ccc8060f84cd
João Teles <104568838+BROOK442@users.noreply.github.com>
2023-04-18 15:04:33 +0000

    quadro

--- main.js
+++ main.js
@@ -6,16 +6,41 @@ containers.forEach(container => {
     const timeline = container.querySelector(".dragbar.timeline")
     const timelineDrag = timeline.querySelector(".draggable")
     const timer = container.querySelector(".timer")
-    btPlayPause.addEventListener("click", () => video.play())
     
-    video.currentTime = 55
+    btPlayPause.addEventListener("click", () => {
+        if (video.paused) {
+            video.play()
+            btPlayPause.innerText = btPlayPause.dataset.pauseIcon
+            return
+        }
+
+        video.pause()
+        btPlayPause.innerText = btPlayPause.dataset.playIcon
+    })
+
+    
+    video.currentTime = 58
 
     video.addEventListener("timeupdate", () => {
         const percent = (video.currentTime / video.duration) * 100
         timelineDrag.style.setProperty("--percent", `${percent}%`) 
-        const min = Math.floor(video.currentTime / 60)
-        const seg = Math.floor(video.currentTime % 60)
-        console.log(seg)
         timer.innerText = Math.floor(video.currentTime)
+
+        if (timer.innerText >= 60) {
+            min = Math.floor(timer.innerText / 60)
+            seg = Math.floor(timer.innerText % 60)
+            timer.innerText = min + ":" + seg
+        }
     })
+
+    const dragbars = container.querySelectorAll(".dragbar")
+            
+    dragbars.forEach(dragbar => {
+        const dragabble = dragbar.querySelector(".draggable")
+        dragbar.addEventListener("click", ev => {
+            const width = Math.floor(dragbar.getBoundingClientRect().width)
+            const percent = (ev.offsetX / width) * 100
+            dragabble.style.setProperty("--percent", `${percent}%`)
+        })
+    })   
 })
