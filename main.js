b36b467bc41f1d991b38e76b9c672f812d4818fa
João Teles <104568838+BROOK442@users.noreply.github.com>
2023-04-25 15:18:31 +0000

    pao

--- main.js
+++ main.js
@@ -19,39 +19,65 @@ containers.forEach(container => {
     })
     
     video.currentTime = 58
-    
+
     video.addEventListener("timeupdate", () => {
         const percent = (video.currentTime / video.duration) * 100
         timelineDrag.style.setProperty("--percent", `${percent}%`)
         timer.innerText = Math.floor(video.currentTime)
 
         if (timer.innerText >= 60) {
-            min = math.floor(timer.innerText / 60)
-            seg = math.floor(timer.innerText % 60)
+            min = Math.floor(timer.innerText / 60)
+            seg = Math.floor(timer.innerText % 60)
+            
+            if (min <= 9){
+                min = "0" + min
+            }
+
+            if (seg <=9){
+                seg = "0" + seg
+            }
+
             timer.innerText = min + ":" + seg
+
         }
     })
+    
+    // -----------------------------------------------------
     
     const dragbars = container.querySelectorAll(".dragbar")
     
     dragbars.forEach(dragbar => {
         const dragabble = dragbar.querySelector(".draggable")
         if (dragbar.classList.contains("volume")) {
-            draggable.style.setProperty("--percent", `100%`)
-        }
-
-        dragbar.addEventListener("click", ev => {
+            dragabble.style.setProperty("--percent", `100%`)
+          }
+  
+          dragbar.addEventListener("mousedown", ev => {
+            dragbar.classList.add("dragging")
+          })
+          
+          dragbar.addEventListener("mouseup", ev => {
+            dragbar.classList.remove("dragging")
+          })
+  
+          dragbar.addEventListener("mouseout", ev => {
+            dragbar.classList.remove("dragging")
+          })
+  
+          dragbar.addEventListener("mousemove", ev => {
+            if (ev.target != dragbar || !dragbar.classList.contains("dragging")) 
+              return
             const width = Math.floor(dragbar.getBoundingClientRect().width)
-            const index = (ev.offsetX / width)
-            const percent = index* 100
+            const index= (ev.offsetX / width)
+            const percent = index * 100
             dragabble.style.setProperty("--percent", `${percent}%`)
 
-            if (dragbar.classList.contains ("timeline")) {
+            if (dragbar.classList.contains("timeline")) {
                 video.currentTime = video.duration * index
                 return
             }
 
-            if (dragbar.classList.contains ("volume")) {
+            if (dragbar.classList.contains("volume")) {
                 video.volume = index
                 return
             }
